b156472e8613f862a99c0828e522ca398d5bb62a
bilalsu <68357165+bilalsu@users.noreply.github.com>
2022-02-22 22:56:39 -0500

    Add files via upload

---
+++ lonleyInteger.py
@@ -0,0 +1,34 @@
+#Given an array of integers, where all elements but one occur twice, find the unique element.
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    lonleyint = (sum((set(a))) * 2) - sum(a) 
+    return lonleyint
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
